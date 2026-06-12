1230431e62bd7a6a3b7de481afb09b2a9c48359c
Felipe Santana <felipeUmbra@users.noreply.github.com>
2026-06-12 12:46:57 -0300

    feat: add service worker configuration and remove unused interfaces file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.fed5oear21k"
+    "revision": "0.pi1r3h1gtfs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
